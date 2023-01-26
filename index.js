4d9f261144aeb173cb2a472ce592ac58c1da23c2
Kauã Miguel <98243777+Kc1t@users.noreply.github.com>
2023-01-26 13:35:42 -0300

    conclusão

--- index.js
+++ index.js
@@ -1,28 +1,51 @@
 const puppeteer = require('puppeteer');
 
-const MOVIE_ID = 'tt1877830'
+const MOVIE_ID = '/tt5807606'
 
-const IMDB_URL = `https://www.imdb.com/title/${MOVIE_ID}/?ref_=fn_al_tt_1`;
+// const IMDB_URL = `https://www.imdb.com/title/${MOVIE_ID}/?ref_=fn_al_tt_1`;
+const IMDB_URL = `https://www.imdb.com/title/tt5807606/?ref_=fn_al_tt_1`;
 
 
 (async () =>{
     
     const browser = await puppeteer.launch({
         headless: false,
-        slowMo: 0 //tempo em para abrir 1s = 1000
-    }); //inicia o navegador
-    const page = await browser.newPage() //abre nova pag
+        slowMo: 0                                                       //tempo em para abrir 1s = 1000
+    });                                                                 //inicia o navegador
+    const page = await browser.newPage()                                //abre nova pag
 
     await page.goto(IMDB_URL, {waitUntil: 'networkidle2'})
 
-    await page.screenshot ( {path: './print.jpeg', fullPage: yes} ) //pega uma print
+    await page.screenshot ( {path: './print.jpeg', /*fullPage: yes*/} ) //pega uma print
  
-    await browser.close()
+    
+    // pegando elemento da pag
+    let data = await page.evaluate(()=>{
 
+        let titulo = document.querySelector('.sc-b73cd867-0.cEmnhL').innerHTML; //pega a classe selecionada e retorna o conteudo dela
+        let nota = document.querySelector('.sc-7ab21ed2-1.eUYAaq').innerHTML 
+
+
+        return{
+            titulo,
+            nota
+        }
+
+    })
+
+    console.log(data)
+
+
+    await browser.close()             //fecha o browser
+    
 })()
 
 
-// Tira Print da Página e abre  pag
+
+
+
+
+// Tira Print da Página e abre pag
 
 // (async ()=>{
 //     const browser = await puppeteer.launch({ //abre o navegador
